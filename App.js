1752d0c5e0d4c517c871add377468a7a7903d1b8
unknown <hunghppd09527@fpt.edu.vn>
2024-02-25 21:46:03 +0700

    update

--- App.js
+++ App.js
@@ -1,22 +1,8 @@
-// import { StatusBar } from 'expo-status-bar';
-// import { StyleSheet, Text, View } from 'react-native';
+ import { StatusBar } from 'expo-status-bar';
+ import { StyleSheet, Text, View } from 'react-native';
 
 import React from 'react';
-import Splash from './src/screen/auth/Splash';
-import SignUp from './src/screen/auth/SignUp';
-import AuthHeader from './components/AuthHeader';
-import Lab5 from './src/Lab/Lab5';
-import Lab6 from './src/Lab/Lab6';
-import Lab7 from './src/Lab/Lab7';
-import TopTab from './src/Lab/Lab7/TopMenu';
-import SignIn from './src/screen/auth/SignIn';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { Image } from 'react-native';
-import Home from './src/screen/app/Home';
-import Profile from './src/screen/app/Profile';
-import Favourites from './src/screen/app/Favourites';
+
 import Crud from './src/Lab/Lab8/CRUD';
 
 // const Stack = createNativeStackNavigator();
